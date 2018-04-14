5590a51d021bc07635a09190d3471c6b73ff5e06
bryan bravo <bryanbravo90@yahoo.com>
2018-04-14 13:33:36 -0700

    removed tslint errors on COI

--- src/app/models/PreAward/ConflictOfInterest.ts
+++ src/app/models/PreAward/ConflictOfInterest.ts
@@ -1,36 +1,32 @@
 import { User } from "../User";
 
 export class ConflictOfInterest{
-	id: number;
-	type: string;
-	progress: number;
-	pI: User;
+    id: number;
+    type: string;
+    progress: number;
+    pI: User;
     proposalNumber: number;
     proposalTitle: string;
-	sponsor: Map<boolean,string>;
-	subAward: boolean;
-	subAwardSponsor: string;
-	subAwardAgency: string;
-	disclosureReasons: Map<boolean,string>;
-	subawardWithFederalAgencyPassThrough: string;
+    sponsor: Map<boolean,string>;
+    subAward: boolean;
+    subAwardSponsor: string;
+    subAwardAgency: string;
+    disclosureReasons: Map<boolean,string>;
+    subawardWithFederalAgencyPassThrough: string;
     budgetPeriodStart: Date;
-	budgetPeriodEnd: Date;
-	projectPeriodStart: Date;
-	projectPeriodEnd: Date;
-	amountRequested: number;
-	iRBACUCIBCNo: number; 
-	significantFinInterest: boolean;
-	piSign: string;
-	piDate: Date;
-	keyPersonnelSign: string; //signature
-	keyPersonnelDate: Date;
-	//anyOtherInvestigator?: boolean;//PI page
-	//otherInvestigators?: string[];//PI page 
-	aRIOfficial: boolean;
-	aRIDate: Date;
-	
-	constructor(id, type){
-		this.id=id;
-		this.type = type;
-		}
+    budgetPeriodEnd: Date;
+    projectPeriodStart: Date;
+    projectPeriodEnd: Date;
+    amountRequested: number;
+    iRBACUCIBCNo: number; 
+    significantFinInterest: boolean;
+    piSign: string;
+    piDate: Date;
+    keyPersonnelSign: string; // signature
+    keyPersonnelDate: Date;
+    // anyOtherInvestigator?: boolean;//PI page
+    // otherInvestigators?: string[];//PI page
+    aRIOfficial: boolean;
+    aRIDate: Date;
+
 }
